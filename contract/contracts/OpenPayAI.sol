ec02891de9a6692f035b5648756b26e5b75189db
Luzian Scherrer <luzian@scherrer.ch>
2025-10-12 10:12:30 +0200

    contract

--- contract/contracts/OpenPayAI.sol
+++ contract/contracts/OpenPayAI.sol
@@ -18,6 +18,49 @@ contract OpenPayAI {
         require(msg.sender == owner, "Not the Owner");
         _;
     }
+    modifier isDataOwner(bytes32 hash) {
+        require(entries[hash].dataOwner == msg.sender, "Not the data owner");
+        _;
+    }
+
+    function addEntry(
+        bytes32 hash,
+        uint256 price,
+        address dataOwner
+    ) external isOwner {
+        require(entries[hash].dataOwner == address(0), "Entry already exists");
+
+        entries[hash] = Entry({price: price, dataOwner: dataOwner});
+    }
+
+    function getEntry(bytes32 hash) external view returns (uint256, address) {
+        require(entries[hash].dataOwner != address(0), "Entry does not exist");
+
+        Entry memory entry = entries[hash];
+        return (entry.price, entry.dataOwner);
+    }
+
+    function buyLicense(bytes32 hash) external payable {
+        require(entries[hash].dataOwner != address(0), "Entry does not exist");
+        require(msg.value >= entries[hash].price, "Insufficient funds");
+
+        address dataOwner = entries[hash].dataOwner;
+        uint256 price = entries[hash].price;
+
+        (bool success, ) = dataOwner.call{value: price}("");
+        require(success, "Transfer failed");
+
+        licenses[msg.sender][hash] = true;
+    }
+
+    function updatePrice(
+        bytes32 hash,
+        uint256 newPrice
+    ) external isDataOwner(hash) {
+        require(entries[hash].dataOwner != address(0), "Entry does not exist");
+
+        entries[hash].price = newPrice;
+    }
 
     function withdraw() external isOwner {
         uint256 balance = address(this).balance;
